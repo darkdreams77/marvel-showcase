e73595309b9ca05ce6fabc3d485b489bd7002775
darkdreams77 <darkdreams77@users.noreply.github.com>
2026-06-12 11:34:48 +0200

    chore: deploy frontend vercel

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,19 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": "http://localhost:3000",
+export default ({ mode }: { mode: string }) => {
+  const env = loadEnv(mode, process.cwd());
+
+  const API_URL = `${env.VITE_BFF_URL ?? "http://localhost:3000"}`;
+
+  return defineConfig({
+    plugins: [react(), tailwindcss()],
+
+    server: {
+      proxy: {
+        "/api": API_URL,
+      },
     },
-  },
-});
+  });
+};
